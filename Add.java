a1116f00864496320964f8fa08f3585cc22522b9
MeenakshiRamu <50766051+MeenakshiRamu@users.noreply.github.com>
2025-06-21 21:03:48 +0530

    Minor Update Add.java

--- Add.java
+++ Add.java
@@ -2,7 +2,8 @@ class Add {
   public static void main(String args[]) {
     int a=5, b=10;
     int c=a+b;
-    System.out.println("The output is:"+c);
+    System.out.println("The output of addition  is:"+c);
+    
   }
 } 
   
